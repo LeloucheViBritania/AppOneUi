468abd6e600fdb1e1b96aa50222f63940432a3fc
LeloucheViBriania <kra.kobenan.olivier@gmail.com>
2020-03-20 22:09:31 +0000

    AppChatUi

---
+++ AppOneUIChat/AppOneUIChat/AppointmentPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppOneUIChat
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppointmentPage : ContentPage
+    {
+        public AppointmentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnComfirm_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new ComfirmPage());
+        }
+    }
+}
